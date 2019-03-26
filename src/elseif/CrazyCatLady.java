3fb7da1d631b5edbf7b8e9a13057285e66a79254
cheetah676 <league@WTS20.attlocal.net>
2019-03-25 18:31:02 -0700

    Finished highlowgame

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,26 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String L = JOptionPane.showInputDialog("How many cat's does thee have?");
 		// 2. Convert their answer into an int
-
+int l = Integer.parseInt(L);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if (l > 2) {
+	JOptionPane.showMessageDialog(null, "crazy cat lady");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+if (l <= 1 && l >= 2) {
+	playVideo("https://youtu.be/5dsGWM5XGdg");
+	}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+else {
+	playVideo("https:// youtu.be/5dsGWM5XGdg");
+	}
 	}
 
 	static void playVideo(String videoURL) {
@@ -29,4 +37,3 @@ public class CrazyCatLady {
 	}
 
 }
-
---
+++ src/elseif/Highlowtest.java
@@ -0,0 +1,24 @@
+
+//Copyright (c) The League of Amazing Programmers 2013-2017
+//Level 0
+
+package elseif;
+
+import java.nio.channels.ShutdownChannelGroupException;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Highlowtest {
+
+public static void main(String[] args) {
+	int a = 1;
+	String l = JOptionPane.showInputDialog("max");
+	int L = Integer.parseInt(l);
+	for (int K = L; K > 2; K = K / 2) {
+		 a = a + 1;
+	}
+	System.out.println(a);
+}
+
+}
