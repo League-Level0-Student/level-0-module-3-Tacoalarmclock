bd9edd74a8d4a03796f5b4dd170203bd6d40b011
githubdoesntwork <league@WTS20.attlocal.net>
2019-04-15 18:30:08 -0700

    :sunglasses: fizz buzz, calculator
    yay

--- src/elseif/HighlowCalculator.java
+++ src/elseif/HighlowCalculator.java
@@ -11,31 +11,20 @@ package elseif;
 import javax.swing.JOptionPane;
 
 public class HighlowCalculator {
-	public static void main() {
 
-		String a = JOptionPane.showInputDialog("Max number");
-		 int m =Integer.parseInt(a);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+public static void main(String[] args) {
+	
+
+	String a = JOptionPane.showInputDialog("Max number");
+		int z = Integer.parseInt(a);
+		while (1 == 1) {
+
+			z = z + 0;
+			String b = JOptionPane.showInputDialog("Current number");
+			String c = JOptionPane.showInputDialog("< or >\n signs");
+			
+		}
+
 	}
 
 }
